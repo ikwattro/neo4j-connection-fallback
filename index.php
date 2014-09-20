8076fdac3797bb7a840e0ef5eadb8edea311127a
Christophe Willemsen <willemsen.christophe@gmail.com>
2014-09-20 11:55:17 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Neoxygen\NeoClient\Client;
+
+$logger = new \Monolog\Logger("neoclient");
+$logger->pushHandler(new \Monolog\Handler\ErrorLogHandler());
+
+$client = new Client();
+$client
+	->loadConfigurationFile(__DIR__.'/neoclient.yml')
+	->setLogger('neolog', $logger)
+	->build();
+
+$query = 'MATCH (actor:Actor) RETURN actor.name ORDER BY actor.name';
+
+$actors = $client->sendCypherQuery($query);
+
+echo '<pre>';
+print_r(json_decode($actors, true));
